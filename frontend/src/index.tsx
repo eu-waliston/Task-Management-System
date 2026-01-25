2b47cea673ff87e424e5550ee812fc080d583dae
Waliston <wesantosdev@gmail.com>
2026-01-25 13:09:44 -0300

    update

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,19 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './slices/authSlice';
+import taskReducer from './slices/taskSlice';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    tasks: taskReducer,
+  },
+});
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
